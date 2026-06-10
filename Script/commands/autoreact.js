ed8bd25cd99d235eccfe74b7b990ee759030df39
akibulislam12313-source <akibulislam12313@gmail.com>
2026-06-10 23:12:20 +0600

    Update autoreact.js

--- Script/commands/autoreact.js
+++ Script/commands/autoreact.js
@@ -1,7 +1,7 @@
 module.exports.config = {
  name: "autoreact",
  version: "1.1.1",
- hasPermission: 0,
+ hasPermission: 2,
  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
  description: "Bot React",
  commandCategory: "No Prefix",
